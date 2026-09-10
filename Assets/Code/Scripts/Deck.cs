02fbf01c9c743729d79a5c56e140d41d7fa34745
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2026-09-10 15:33:40 +0200

    Deck prefab

--- Assets/Code/Scripts/Deck.cs
+++ Assets/Code/Scripts/Deck.cs
@@ -1,8 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
+    [SerializeField] Card[] initialCards;
     private Queue<Card> deckCards;
-    private Card.Suit triumphSuit; 
+    private Card.Suit triumphSuit;
+
+    void Start()
+    {
+        CreateDeck();
+    }
+
+    private void CreateDeck()
+    {
+        foreach(Card card in initialCards)
+        {
+            deckCards.Enqueue(card);
+        }
+    }
 }
