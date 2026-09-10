0e4e6faa16a5b2f791eae62cfc04aecaf3c994fc
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2026-09-10 13:07:01 +0200

    Card scriptable object

---
+++ Assets/Code/Scripts/Card.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "Scriptable Objects/Card")]
+public class Card : ScriptableObject
+{
+    [System.Serializable]
+    public enum Suit
+    {
+        Clubs,
+        Cups,
+        Golds,
+        Swords
+    }
+    [SerializeField] private Texture2D cardImage;
+    [SerializeField] private Suit cardSuit;
+    [Range(1, 12)]
+    [SerializeField] private int number;
+
+    /// <summary>
+    /// Calculates the card's value based on the card number and the rules.
+    /// </summary>
+    private int CalculateCardValue()
+    {
+        switch (number)
+        {
+            case 1:
+                return 11;
+            case 3:
+                return 10;
+            case 12:
+                return 4;
+            case 11:
+                return 3;
+            case 10:
+                return 2;
+        }
+
+        return 0;
+    }
+}
